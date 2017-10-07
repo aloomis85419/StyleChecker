8dea404007dc1908a99a3db177fe7af72eaf38c7
aloomis85419 <aloomis85419@gmail.com>
2017-10-07 01:38:43 -0600

    Fully funtional method curly brace checker. Need to add abilit to check closing brace alignment.

--- src/MethodBraceAlignedLeftChecker.java
+++ src/MethodBraceAlignedLeftChecker.java
@@ -7,26 +7,25 @@ import java.util.regex.Pattern;
  * Created by aloom on 10/6/2017.
  */
 public class MethodBraceAlignedLeftChecker extends StyleChecker{
+    //WORK IN PROGRESS.
     int alignPosition = 0;
     int matchPosition = 0;
+    int nextLineNum;
     public void methodBracesAlignedLeft(String currentLine, String nextLine, int lineCount){
         MethodIndentationChecker methodRegexSearcher = new MethodIndentationChecker();
+        nextLineNum = lineCount+1;
         if (methodRegexSearcher.methodRegexMatcher(currentLine) == true) {
                 if(currentLine.contains("{")){
-                   errorTrace("Line "+lineCount+": ","Incorrect method brace alignment. Opening brace is on the same line as method declaration.");
+                   errorTrace("Line "+lineCount+": ","Incorrect method brace alignment. Opening brace is on the same line as method declaration.\n");
                 }
                 if(nextLine.isEmpty()){
-                    errorTrace("Line "+lineCount+1+": ","Blank line between method declaration and opening brace.");
+                    errorTrace("Line "+nextLineNum+": ","Blank line between method declaration and opening brace.\n");
                 }
         }
         if(nextLine.contains("{") && methodRegexSearcher.methodRegexMatcher(currentLine) == true){
             Character currentChar;
             for(int i = 0; i < currentLine.length(); i++){
                 currentChar = currentLine.charAt(i);
-                if(!currentLine.substring(0,3).equals("    ")){
-                    errorTrace("Line "+lineCount+": ","Incorrect indentation/spacing of method declaration.");
-                    break;
-                }
                 if(currentChar == ' '){
                     continue;
                 }
@@ -35,27 +34,23 @@ public class MethodBraceAlignedLeftChecker extends StyleChecker{
                     break;
                 }
             }
-            System.out.println("Position of first character of method declaration: LineCount: "+lineCount+" Index within Line: "+alignPosition);
             for (int i = 0; i < nextLine.length(); i++){
                 currentChar = nextLine.charAt(i);
                 if(currentChar == '{'){
                     matchPosition = i;
                     break;
                 }
-                else{
-                    errorTrace("Line "+lineCount+1+": ","Line did not contain opening curly bracket after method declaration.");
-                }
             }
             if (alignPosition != matchPosition){
                 int spaceDif;
                 if(matchPosition > alignPosition){
                     spaceDif = matchPosition - alignPosition;
-                    errorTrace("Line "+lineCount+1+": ","Incorrect method brace alignment. Curly braces must be placed under the first letter of a method declaration. "
+                    errorTrace("Line "+nextLineNum+": ","Incorrect method brace alignment. Curly braces must be placed under the first letter of a method declaration.\n"
                             +"Curly brace is "+spaceDif+" spaces to far to the right.");
                 }
                 else if (matchPosition < alignPosition){
-                    spaceDif = matchPosition - alignPosition;
-                    errorTrace("Line "+lineCount+1+": ","Incorrect method brace alignment. Curly braces must be placed under the first letter of a method declaration. "
+                    spaceDif = alignPosition - matchPosition;
+                    errorTrace("Line "+nextLineNum+": ","Incorrect method brace alignment. Curly braces must be placed under the first letter of a method declaration.\n"
                     +"Curly brace is "+spaceDif+" spaces to far to the left.");
                 }
             }
